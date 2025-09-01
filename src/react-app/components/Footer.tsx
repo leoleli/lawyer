5ac5ed281b5e3ff686087d031afbd4c627847da5
leoleli <leoleli3@outlook.com>
2025-09-01 16:25:01 -0300

    Updated Footer.tsx

--- src/react-app/components/Footer.tsx
+++ src/react-app/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Phone, Mail, MapPin, Facebook, Instagram, Linkedin, Youtube } from 'lucide-react';
+import { Phone, Mail } from 'lucide-react';
 
 export default function Footer() {
   const services = [
